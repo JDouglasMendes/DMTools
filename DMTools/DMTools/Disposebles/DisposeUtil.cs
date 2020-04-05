31ebba7b79d261624354105df0fc860fec3b3455
Douglas Mendes <douglasaleixomendes@gmail.com>
2020-04-05 00:36:47 -0300

    Add feature dispose all objects in parans array

---
+++ DMTools/DMTools/Disposebles/DisposeUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMTools.Disposebles
+{
+    public static class DisposeUtil
+    {
+        public static void Disposables(params IDisposable[] disposables) => Array.ForEach(disposables, x => x.Dispose());
+    }
+}
